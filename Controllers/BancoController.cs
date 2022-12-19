53d3af4be02563b36e7269f0393de59302ff4599
Leonardo <lleonardo1555@gmail.com>
2022-12-18 21:08:38 -0300

    First Commit

---
+++ Controllers/BancoController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegraBrasil.Controller
+{
+    [ApiController]
+    [Route("api/v1/controller")]
+    public class BancoController : ControllerBase
+    {
+
+
+    }
+}
---
+++ Services/BancoService.cs
@@ -0,0 +1,7 @@
+﻿namespace IntegraBrasilApi.Services
+{
+    public class BancoService
+    {
+
+    }
+}
